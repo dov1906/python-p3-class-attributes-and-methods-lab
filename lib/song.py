4980a355a26dcf1fd26237296f60d399d6e0f75a
Dov Azogui <dov.azogui@gmail.com>
2024-10-09 17:28:32 -0400

    updated the Song Class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,33 @@
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+    artist_count = {}
+    genre_count = {}
+    
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+        
+
+        
+    
+
+
+    
+    
